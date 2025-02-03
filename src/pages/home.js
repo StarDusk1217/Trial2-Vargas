ce27eb0c5d6d855eea436b9c1d71491d7f2ea33c
StarDusk1217 <starmiguel17@gmail.com>
2025-02-03 19:39:29 +0800

    2nd commit

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function home() {
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Text>Allen Miguel L. Vargas</Text>
+    </Box>
+  )
+}
+
+export default home;
